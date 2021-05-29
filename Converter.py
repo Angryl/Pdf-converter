a09598d2d6251c207bff4862d90d011882071d02
Rudra Pratap Singh <30315987+Angryl@users.noreply.github.com>
2021-05-30 00:16:08 +0530

    Create Converter.py
    write the pdf converter script

---
+++ Converter.py
@@ -0,0 +1,7 @@
+#python small script to convert Docx files to pdf files
+from docx2pdf import convert
+
+#convert ("source files of docs", "output file location");
+convert("C:/Users/Nobody/Documents/c program/file/","C:/Users/Nobody/Documents/c program/pdf/");
+
+#program finish
